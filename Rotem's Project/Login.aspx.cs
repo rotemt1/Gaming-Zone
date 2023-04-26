29aa69a19003c17f368883ebe49973cc67c1571b
Rotem Tater <129662982+rotemt1@users.noreply.github.com>
2023-04-26 16:28:08 +0300

    Add files via upload

---
+++ Rotem's Project/Login.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Login : System.Web.UI.Page
+{
+    public string response = "", userName, password, selectQuery = "";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.Form["submit"] != null)
+        {
+
+            userName = Request.Form["userName"];
+            password = Request.Form["password"];
+
+
+            selectQuery = "select * from Users where userName='" + userName + "' and password='" + password + "';";
+            bool x = MyAdoHelper.IsExist(selectQuery);
+            if (x)
+            {
+                MasterPage.logedInUsr = userName;
+                Session["userName"] = userName;
+                Response.Redirect("Home.aspx");
+                  
+
+            }
+            else
+                response = "You are not signed in, please register";
+        }
+
+    }
+}
---
+++ Rotem's Project/Register.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Register : System.Web.UI.Page
+{
+    public string form_response = " ", selectQuery = "", userName, password, fName, lName, email, gender;
+    int age = 0;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string sql_command;
+        if (IsPostBack)
+        {
+            userName = Request.Form["userName"];
+            password = Request.Form["password"];
+            fName = Request.Form["fName"];
+            lName = Request.Form["lName"];
+            email = Request.Form["email"];
+            if (Request.Form["age"] != "")
+                age = Int32.Parse(Request.Form["age"]);
+            gender = Request.Form["gender"];
+            selectQuery = "select * from Users where userName='" + userName + "';";
+            bool isUserExist = MyAdoHelper.IsExist(selectQuery);
+            if (!isUserExist)
+            {
+                sql_command = "insert into Users values( '" + fName + "','" + lName + "','" + userName + "','" + password + "','" + email + "','" + age + "','" + gender + "')";
+                int x = MyAdoHelper.RowsAffected(sql_command);
+                if (x == 1)
+                    form_response = "<font color=\"green\"> Successful insertion";
+                else
+                    Response.Write("Unsuccessful insertion");
+            }
+            else
+                form_response = "<font color=\"red\"> There is already user name :" + userName;
+
+
+        }
+    }
+}
